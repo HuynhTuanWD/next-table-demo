3686d21a482a2316d73fd36b030e0bae17a1ac79
Huynh Tuan <huynhtuan.wd@gmail.com>
2022-04-17 02:29:50 +0700

    init

---
+++ components/DemoTable.js
@@ -0,0 +1,49 @@
+import { Table } from 'antd';
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+const DemoTable = ({ updateSingleData }) => (
+  <Table
+    onRow={(record, rowIndex) => {
+      return {
+        onClick: (event) => {
+          updateSingleData(record);
+        },
+      };
+    }}
+    dataSource={dataSource}
+    columns={columns}
+  />
+);
+
+export default DemoTable;
